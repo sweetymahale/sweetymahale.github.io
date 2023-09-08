3aeed255ed4ab22b4ff9e9b90b0e13d982fad33d
Sweety Mahale <mahalesweety18@gmail.com>
2023-09-08 15:04:14 -0500

    added code for routing

---
+++ src/NavBar.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { NavLink, Outlet } from 'react-router-dom';
+import "./styles.css"
+
+export default function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        SM
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
+        {/* hamburger svg code... */}
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+        <Outlet />
+      </div>
+    </nav>
+    );
+}
