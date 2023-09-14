27030d44c403721ef24b7139f46962f4b9a74fa2
Sweety Mahale <mahalesweety18@gmail.com>
2023-09-13 23:57:10 -0500

    added drag and drop example

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import TrafficLight from './TrafficLight';
 import './styles.css';
 import TodoList from './TodoList';
 import ContactForm from './ContactForm';
+import DragnDrop from "./DragnDrop";
 
 const config = {
   red: {
@@ -34,7 +35,13 @@ export default function Home() {
             <p>    
                 I am interested in working on the latest technologies, using my skills and knowledge imporove the performance of application.
             </p>
+            <p>
+                More content coming soon...
+            </p>
         </span>
+        <Row gutter={40} justify="left" align="bottom">
+          <Col  xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 12 }}lg={{ span: 12 }} xl={{ span: 12 }}> <div className="wrapper example-header">Drag and Drop<DragnDrop /></div></Col>
+        </Row>
         <Row gutter={40} justify="left" align="bottom">
           <Col xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 6 }}> <div className="wrapper example-header" >Traffic Signal<TrafficLight config={config} /></div></Col>
           <Col  xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 6 }}lg={{ span: 6 }} xl={{ span: 6 }}> <div className="wrapper example-header">ToDo List<TodoList/></div></Col>
@@ -42,7 +49,6 @@ export default function Home() {
         <Row gutter={40} justify="left" align="bottom">
           <Col  xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 6 }}lg={{ span: 6 }} xl={{ span: 6 }}> <div className="wrapper example-header">Contact Form<ContactForm /></div></Col>
         </Row>
-    </div>
-    
+    </div>    
   )
 }
