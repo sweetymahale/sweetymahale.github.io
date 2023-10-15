6555d4337da40fb4564427075d96dd4396ac069d
Sweety Mahale <mahalesweety18@gmail.com>
2023-10-14 22:00:52 -0500

    Added TVShowListItems

--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import s from "./style.module.css";
 import { TVShowAPI } from './tv-show';
-import { BASE_URL } from "../config";
-import { BACKDROP_BASE_URL } from "../config";
+import { BASE_URL } from "./config";
+import { BACKDROP_BASE_URL } from "./config";
 import { TVShowDetail } from "./TVShowDetail/TVShowDetails";
 import { Logo } from "./Logo/Logo";
 import logoImg from "./img/logo.png";
 
 export function MovieList() {
     const [currentTvShow, setCurrentTVShow] = useState();
+    const [recommendationList, setRecommendationList] = useState([]);
     async function fetchPopulars() {
         const popularTVShowList = await TVShowAPI.fetchPopulars();
         if (popularTVShowList.length > 0)
@@ -20,6 +21,31 @@ export function MovieList() {
         fetchPopulars();
 
     }, []);
+    useEffect(() => {
+        if (currentTVShow) {
+            fetchRecommendations(currentTVShow.id);
+        }
+    }, [currentTvShow]);
+
+
+    async function fetchRecommendations(tvShowId) {
+        const recommendationListResp = await TVShowAPI.fetchRecommendations(
+            tvShowId
+        );
+        if (recommendationListResp.length > 0) {
+            setRecommendationList(recommendationListResp.slice(0, 10));
+        }
+    }
+    function updateCurrentTVShow(tvShow) {
+        setCurrentTVShow(tvShow);
+    }
+
+    async function fetchByTitle(title) {
+        const searchResponse = await TVShowAPI.fetchByTitle(title);
+        if (searchResponse.length > 0) {
+            setCurrentTVShow(searchResponse[0]);
+        }
+    }
     console.log(currentTvShow);
     return (
         <div className={s.main_container}
@@ -41,7 +67,14 @@ export function MovieList() {
             <div className={s.tv_show_detail}>
                 {currentTvShow && <TVShowDetail tvShow={currentTvShow} />}
             </div>
-            <div className={s.recommended_tv_show}>Recommended Tv SHow</div>
+            <div className={s.recommended_shows}>
+                {currentTvShow && (
+                    <TVShowList
+                        onClickItem={updateCurrentTVShow}
+                        tvShowList={recommendationList}
+                    />
+                )}
+            </div>
         </div>
     )
 }
--- src/MovieList/config.js
+++ src/MovieList/config.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY_PARM = '?api_key=0df0069d1dfc92cf0483461bcd1e3249';
 const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original';
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
 
-export { BASE_URL, API_KEY_PARM, BACKDROP_BASE_URL };
+export { BASE_URL, API_KEY_PARM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
 
